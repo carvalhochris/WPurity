e80d0248f1d8e5e87287112f16e4a1fdc279e04b
Christopher <kriss17388+dev@gmail.com>
2023-03-05 15:28:44 +0000

    paragraphs and headings

--- feed/serializers.py
+++ feed/serializers.py
@@ -8,20 +8,51 @@ class WordPressSerializer(serializers.Serializer):
     content = serializers.SerializerMethodField()
 
     def get_content(self, obj):
-        # Use BeautifulSoup to parse the WordPress HTML content
+    # Use BeautifulSoup to parse the WordPress HTML content
         soup = BeautifulSoup(obj['content']['rendered'], 'html.parser')
 
-        # Extract the plain text from the parsed HTML
-        plain_text = soup.get_text()
+        # Initialize an empty list to store the JSON objects for each heading
+        headings = []
 
-        # Use strip_tags to remove any remaining HTML tags
-        content = strip_tags(plain_text)
+        # Find all heading elements (h1, h2, h3, etc.) in the parsed HTML
+        for heading in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
+            # Create a JSON object for the heading with the tag and text attributes
+            heading_json = {
+                'tag': heading.name,
+                'text': heading.get_text(strip=True),
+                'paragraphs': []
+            }
 
-        # Replace all instances of "\n" with an empty string
-        content = content.replace("\n", " ")
+            # Find all paragraph elements following the current heading element
+            next_element = heading.find_next_sibling()
+            while next_element is not None and next_element.name not in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']:
+                # Extract the plain text from the paragraph element
+                plain_text = next_element.get_text(strip=True)
 
-        # Return the sanitized content
-        return content
+                # Use strip_tags to remove any remaining HTML tags
+                paragraph = strip_tags(plain_text)
+
+                # Replace all instances of "\n" with an empty string
+                paragraph = paragraph.replace("\n", " ")
+
+                # Add the paragraph to the current heading's list of paragraphs
+                heading_json['paragraphs'].append(paragraph)
+
+                # Move to the next sibling element
+                next_element = next_element.find_next_sibling()
+
+            # Add the current heading to the list of headings
+            headings.append(heading_json)
+
+        # Create a JSON object for the content with the headings and paragraphs
+        content_json = {
+            'headings': headings
+        }
+
+        # Return the content JSON object
+        return content_json
+
+
 
     def get_slug(self, obj):
         # Retrieve the slug value from the WordPress API response
