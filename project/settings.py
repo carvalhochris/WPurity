ed7d2e4868202a13de8ed4203be281108510aaa3
Christopher <kriss17388+dev@gmail.com>
2023-03-05 12:55:15 +0000

    allow all origin

--- project/settings.py
+++ project/settings.py
@@ -66,6 +66,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True  # Allow requests from any domain
+
 CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
 
 ROOT_URLCONF = 'project.urls'
